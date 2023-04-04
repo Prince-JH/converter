fec086d467df9346464015dec53652211749913b
jihoon <jihoon.kim@makinarocks.ai>
2023-04-04 17:58:07 +0900

    feat: hide oepnai key

--- dto_converter/convert.py
+++ dto_converter/convert.py
@@ -1,13 +1,11 @@
 import openai
 
-# openai API 키 인증
-openai.api_key = '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'
+openai.api_key = ''
 
 
 def query(statement):
     model = "gpt-3.5-turbo"
 
-    # 메시지 설정하기
     messages = [
         {"role": "user",
          "content": f"Make me JPA DTO class able to carry following json format. "
@@ -15,7 +13,6 @@ def query(statement):
                     f"{statement}"}
     ]
 
-    # ChatGPT API 호출하기
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages
